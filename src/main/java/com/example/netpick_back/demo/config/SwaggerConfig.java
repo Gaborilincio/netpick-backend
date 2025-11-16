7e254d8a5e1bfa8d0f1b0fdae250ab19a9ae30c9
Gaborilincio <ga.mendez@duocuc.cl>
2025-11-16 03:38:57 +0000

    Configuracion de app.properties y creacion de archivos docker

---
+++ src/main/java/com/example/netpick_back/demo/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.example.netpick_back.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(){
+        return new OpenAPI().info(
+            new Info()
+            .title("API netpicck")
+            .version("0.1")
+            .description("Api pruebas")
+        );
+    }
+
+}
