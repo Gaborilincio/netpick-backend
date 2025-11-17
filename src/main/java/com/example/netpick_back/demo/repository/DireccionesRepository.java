493b8a51367653b3c6f6540173651b1449577203
Gaborilincio <ga.mendez@duocuc.cl>
2025-11-17 17:32:21 -0300

    Direcciones service esta listo

--- src/main/java/com/example/netpick_back/demo/repository/DireccionesRepository.java
+++ src/main/java/com/example/netpick_back/demo/repository/DireccionesRepository.java
@@ -1,14 +1,16 @@
 package com.example.netpick_back.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.example.netpick_back.demo.model.Direcciones;
 
 @Repository
 public interface DireccionesRepository extends JpaRepository<Direcciones, Integer> {
 
-    List<Direcciones> findByDireccionesId (Integer direccionId);
+    Optional<Direcciones> findByIdDireccion (Integer idDireccion);
     List<Direcciones> findByCorreoUsuario (Integer usuarioCorreo);
 }
--- src/main/java/com/example/netpick_back/demo/service/DireccionesService.java
+++ src/main/java/com/example/netpick_back/demo/service/DireccionesService.java
@@ -1,5 +1,54 @@
 package com.example.netpick_back.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.netpick_back.demo.model.Direcciones;
+import com.example.netpick_back.demo.repository.DireccionesRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+@SuppressWarnings("null")
 public class DireccionesService {
 
+    @Autowired
+    private DireccionesRepository direccionesRepository;
+
+    public List<Direcciones> findAll() {
+        return direccionesRepository.findAll();
+    }
+
+    public Direcciones findById(Integer id) {
+        Direcciones direcciones = direccionesRepository.findByIdDireccion(id).orElse(null);
+        return direcciones;
+    }
+
+    public Direcciones save(Direcciones direcciones) {
+        return direccionesRepository.save(direcciones);
+    }
+
+    public Direcciones partialUpdate(Direcciones direcciones){
+        Direcciones existingDirecciones = direccionesRepository.findByIdDireccion(direcciones.getIdDireccion()).orElse(null);
+        if (existingDirecciones != null) {
+            if (direcciones.getDireccion()!= null) {
+                existingDirecciones.setDireccion(direcciones.getDireccion());
+            }
+            if (direcciones.getCodigoPostal()!= null){
+                existingDirecciones.setCodigoPostal(direcciones.getCodigoPostal());
+            }
+            if (direcciones.getPais()!= null){
+                existingDirecciones.setPais(direcciones.getPais());
+            }
+            return direccionesRepository.save(existingDirecciones);
+        }
+        return null;
+    }
+
+    public void deleteById(Integer id) {
+        direccionesRepository.deleteById(id);
+    }
 }
