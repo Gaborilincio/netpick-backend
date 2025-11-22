1bbbd42047b648a22007d4836c3125244f1eb1dc
Gaborilincio <ga.mendez@duocuc.cl>
2025-11-22 02:41:20 -0300

    pequeñas correcciones v2

--- src/main/java/com/example/netpick_back/demo/repository/VentaProductosRepository.java
+++ src/main/java/com/example/netpick_back/demo/repository/VentaProductosRepository.java
@@ -1,5 +1,11 @@
 package com.example.netpick_back.demo.repository;
 
-public class VentaProductosRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.netpick_back.demo.model.VentaProductos;
+
+@Repository
+public interface VentaProductosRepository extends JpaRepository<VentaProductos, Integer> {
 
 }
