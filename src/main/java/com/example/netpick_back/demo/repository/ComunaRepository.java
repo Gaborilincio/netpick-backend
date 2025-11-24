4e2754fe8ea403b875a94b11f297bb2621a286b9
POAPCA02LC0811 <SSDD@POAPCA02LC0811.duoc.acad>
2025-11-24 16:38:00 -0300

    UwU

--- src/main/java/com/example/netpick_back/demo/repository/ComunaRepository.java
+++ src/main/java/com/example/netpick_back/demo/repository/ComunaRepository.java
@@ -6,10 +6,14 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.netpick_back.demo.model.Comuna;
+import com.example.netpick_back.demo.model.Direcciones;
+import com.example.netpick_back.demo.model.Usuario;
 
 @Repository
 public interface ComunaRepository extends JpaRepository<Comuna, Integer> {
 
     Optional<Comuna> findByIdComuna (Integer idComuna);
     List<Comuna> findByNombre (String nombre);
+    List<Direcciones> findByUsuario(Usuario usuario);
+
 }
