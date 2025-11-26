d2df8d257a4488fa56920a3c28e1bf5ab16edb87
Gaborilincio <ga.mendez@duocuc.cl>
2025-11-25 23:50:30 -0300

    ola

--- src/main/java/com/example/netpick_back/demo/config/SecurityConfig.java
+++ src/main/java/com/example/netpick_back/demo/config/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.example.netpick_back.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer; // Importante para .cors(Customizer.withDefaults())
+import org.springframework.security.config.Customizer; 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,7 +20,7 @@ public class SecurityConfig {
             
             .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/register", "/login", "/api/v1/producto/**").permitAll()
+                .requestMatchers("/api/v1/auth/**", "/api/v1/producto/**").permitAll()
                 .requestMatchers("/doc/**", "/v3/api-docs/**").permitAll()
 
                 .anyRequest().authenticated()
